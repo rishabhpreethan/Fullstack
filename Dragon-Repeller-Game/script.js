b8839f71b235797c54148005d8bae86535a5596f
rishabhpreethan <rishabh.preethan@gmail.com>
2022-12-11 22:50:56 +0530

    committed

---
+++ Dragon-Repeller-Game/script.js
@@ -0,0 +1,22 @@
+//comments here are just as java
+
+let xp=0;
+let health=100;
+let gold=50;
+let currentWeapon=0;
+let fighting;
+let monsterHealth;
+let inventory=["stick","dagger","sword"];
+
+const button1=document.querySelector("#button1");
+const button2=document.querySelector("#button2");
+const button3=document.querySelector("#button3");
+const text=document.querySelector("#text");
+const xpText=document.querySelector("#xpText");
+const healthText=document.querySelector("#healthText");
+const goldText=document.querySelector("#goldText");
+const monsterStats=document.querySelector("#monsterStats");
+const monsterName=document.querySelector("#monsterName");
+const mosnterHealth=document.querySelector("#monsterHealth");
+
+button1.onclick=goStore
